0bb7ccfdef07cf7047eea482d445099ae10fd2c4
Dominc Wang <ziming0425@icloud.com>
2025-03-29 20:21:51 +0800

    Initial commit

---
+++ src/main/java/UserController.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;/**
+ * @Author Dominic Wang
+ * @Date 2025/3/29 16:02
+ * @version 1.0
+ * @注释
+ */public class UserController {
+}
---
+++ src/main/java/UserRepository.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;/**
+ * @Author Dominic Wang
+ * @Date 2025/3/29 15:46
+ * @version 1.0
+ * @注释
+ */public interface UserRepository {
+}
